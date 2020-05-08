35b6310e281ade5b28e08cda886a35089146cba0
baranaydin <baran.aydin@fibabanka.com.tr>
2020-05-08 18:45:42 +0300

    asd4

--- src/store/actions/commonActions.ts
+++ src/store/actions/commonActions.ts
@@ -1,28 +1,16 @@
-import { ActionCreator } from '../models/action';
+import {ActionCreator} from '../models/action';
 
 export enum COMMON_ACTION_TYPE {
   SHOW_LOADING = '@@common/SHOW_LOADING',
   HIDE_LOADING = '@@common/HIDE_LOADING',
-  new = '@@common/new',
 }
-
-const a = () => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve()
-  }, 5000);
-});
 
 export const showLoading: ActionCreator = (actionType) => ({
   type: COMMON_ACTION_TYPE.SHOW_LOADING,
-  payload: { actionType }
+  payload: {actionType}
 });
 
 export const hideLoading: ActionCreator = (actionType) => ({
   type: COMMON_ACTION_TYPE.HIDE_LOADING,
-  payload: { actionType }
-});
-
-export const newAction: ActionCreator = () => ({
-  type: COMMON_ACTION_TYPE.new,
-  payload: a()
+  payload: {actionType}
 });
